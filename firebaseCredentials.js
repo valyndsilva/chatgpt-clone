df4b86dff58d2b4eac93bfc8cc77ff9c4801007e
valyndsilva <valyndsilva@gmail.com>
2023-03-24 14:58:55 +0000

    firebaseAdmin config

---
+++ firebaseCredentials.js
@@ -0,0 +1,6 @@
+export const FIREBASE_API_KEY = "AIzaSyDdm8pNWZ03UGdqPGszfdzboy68HykA5zs";
+export const FIREBASE_AUTH_DOMAIN = "chatgpt-clone-cba9d.firebaseapp.com";
+export const FIREBASE_PROJECT_ID = "chatgpt-clone-cba9d";
+export const FIREBASE_STORAGE_BUCKET = "chatgpt-clone-cba9d.appspot.com";
+export const FIREBASE_MESSAGING_SENDER_ID = "886038362633";
+export const FIREBASE_APP_ID = "1:886038362633:web:74819ee802b5a7568816d8";
